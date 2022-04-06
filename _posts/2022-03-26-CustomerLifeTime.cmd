559184e3ddaad2cb5ed3818b48403c3b5e203d05
Sebo <sebastian.domsch@gmail.com>
2022-04-06 15:39:32 +0200

    Update 2022-03-26-CustomerLifeTime.cmd

--- _posts/2022-03-26-CustomerLifeTime.cmd
+++ _posts/2022-03-26-CustomerLifeTime.cmd
@@ -5,6 +5,10 @@ layout: post
 categories: media
 ---
 
+![Swiss Alps](https://user-images.githubusercontent.com/4943215/55412536-edbba180-5567-11e9-9c70-6d33bca3f8ed.jpg)
+
+
+
 ## Project Overview
 >In marketing, customer lifetime value (CLV or often CLTV), lifetime customer value (LCV), or life-time value (LTV) is a prognostication of the net profit contributed to the whole future relationship with a customer. The prediction model can have varying levels of sophistication and accuracy, ranging from a crude heuristic to the use of complex predictive analytics techniques. One of the major uses of CLV is customer segmentation, which starts with the understanding that not all customers are equally important. CLV-based segmentation model allows the company to predict the most profitable group of customers, understand those customers' common characteristics, and focus more on them rather than on less profitable customers. CLV-based segmentation can be combined with a Share of Wallet (SOW) model to identify "high CLV but low SOW" customers with the assumption that the company's profit could be maximized by investing marketing resources in those customers. [**Ref:** https://en.wikipedia.org/wiki/Customer_lifetime_value]
 
